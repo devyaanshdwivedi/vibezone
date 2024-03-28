2f008c28279b3c18505300626eb9ecb3f76e3099
devyaanshdwivedi <devyaanshdwivedi1234@gmail.com>
2024-03-28 21:11:08 +0530

    first commit

---
+++ components/layout/TopBar.jsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { SignOutButton, SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import { Add, Logout, Person, Search } from "@mui/icons-material";
+import Image from "next/image";
+import Link from "next/link";
+
+const TopBar = () => {
+  const router = useRouter()
+  const [search, setSearch] = useState('')
+  return (
+    <div className='flex justify-between items-center mt-6'>
+      <div className='relative'>
+        <input type='text' className='search-bar' placeholder='Search posts, people, ...' value={search} onChange={(e) => setSearch(e.target.value)} />
+        <Search className="search-icon" onClic={() => { }} />
+      </div>
+      <button className="create-post-btn" onClick={() => router.push("/create-post")}>
+        <Add /> <p>Create A Post</p>
+      </button>
+      <div className="flex gap-3">
+        <SignedIn>
+          <SignOutButton>
+            <div className="flex cursor pointer items-center md:hidden">
+              <Logout sx={{ color: "white", fontSize: "32px" }} />
+
+            </div>
+          </SignOutButton>
+
+        </SignedIn>
+
+        <Link href="/">
+        <Image src="/assets/dev.jpg" alt="profile photo" width={50} height={50} className="rounded-full md:hidden" />
+      </Link>
+      </div>
+      
+    </div>
+  );
+};
+
+export default TopBar
